11de112d1228c79305234f126319898e2540d031
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-09 11:24:56 -0600

    debugging

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,50 @@
 // dependencies
 const router = require("express").Router();
+// const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
-const saveData = require("../lib/saveData");
 
 // GET request
 router.get("/notes", (req, res) => {
-  saveData
-    .addNote(req.body)
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
+ fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+     if (err) throw err;
+     const notes = JSON.parse(data);
+     res.json(notes);
+ })
 });
 
 // POST request
 router.post("/notes", (req, res) => {
-  saveData
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
+ fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+     if (err) throw err;
+     const notes = JSON.parse(data);
+     const newNote = req.body;
+     newNote.id = uuidv4();
+     notes.push(newNote);
+
+     const CreateNote = JSON.stringify(notes);
+     fs.writeFile(path.join(__dirname, "../db/db.json"), CreateNote, (err) => {
+         if (err) throw err;
+     });
+     res.json(newNote);
+ });
 });
 
 // DELETE request
 router.delete("/notes/:id", (req, res) => {
-  saveData
-    .deleteNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
+ const noteId = req.params.id;
+ fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+     if (err) throw err;
+     const notes = JSON.parse(data);
+     const notesArray = notes.filter(item => {
+         return item.id !== noteId
+     });
+     fs.writeFile('../db/db.json', JSON.stringify(notesArray), (err, data) => {
+         console.log("Deleted note.")
+         if (err) throw err;
+         res.json(notesArray)
+     });
+ });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 // dependencies
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // point server to route files
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
+// set PORT
+const PORT = process.env.PORT || 3003;
+
 // tells node we are creating an 'express' server
 const app = express();
 
-// set PORT
-const PORT = process.env.PORT || 3002;
-
+app.use(express.static(path.join(__dirname, "public")));
 // middleware for 'express' to handle data parsing for string or array data
 app.use(express.urlencoded({ extended: true }));
-
 // parse incoming JSON data
 app.use(express.json());
-
-app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
